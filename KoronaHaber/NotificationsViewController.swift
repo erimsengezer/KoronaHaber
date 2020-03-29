58774de7f5f80eae41d32f88ff45397d2ec85e40
Erim <erimsengezer@gmail.com>
2020-03-30 01:58:07 +0300

    feat : Added table view to NotificationsViewController

--- KoronaHaber/NotificationsViewController.swift
+++ KoronaHaber/NotificationsViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class NotificationsViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         print("Notifications View Controller")
+        configureTableView()
+    }
+    
+    private func configureTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
 
+}
+
+extension NotificationsViewController : UITableViewDelegate {
+    
+}
+
+extension NotificationsViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Test"
+        return cell
+    }
+    
+    
 }
